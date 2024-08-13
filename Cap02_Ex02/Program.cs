e7d73c52586336b6335206e4db468b08f3c7564d
rodrigosenac <digobatata@gmail.com>
2024-08-13 16:05:20 -0300

    Upload dos arquivos

---
+++ Cap02_Ex02/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cap02_Ex02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, R;
+
+            Console.Write("Entre o 1o. valor: ");
+            A = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre o 2o. valor: ");
+            B = int.Parse(Console.ReadLine());
+
+            R = A + B;
+
+            Console.WriteLine();
+            Console.WriteLine("Resultado = " + R);
+
+            Console.WriteLine();
+            Console.WriteLine("Tecla <Enter> para continuar... ");
+            Console.ReadLine();
+
+        }
+    }
+}
